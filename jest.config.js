80984d6da07cc2f704ad5b48c1fe93e28e682a08
Gary Bortosky <garybort@gmail.com>
2022-12-05 16:29:01 -0600

    update workspace

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testPathIgnorePatterns: ['/node_modules/', '/output/', '/packages/gb-xmi-cli/'],
-  globals: {
-    skipBabel: true
-  }
-};
